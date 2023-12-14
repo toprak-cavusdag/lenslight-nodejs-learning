b8dc618f940a4a844a6d08d80c5b344dbdc8f7bd
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-12-14 11:02:47 +0300

    lesson-2

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Index Sayfası'));
+// EJS template Engine
+
+app.set('view engine', 'ejs');
+
+// Static Files middleware
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/about', (req, res) => res.render('about'));
 
 app.listen(port, () => console.log(`Application running on ${port}`));
