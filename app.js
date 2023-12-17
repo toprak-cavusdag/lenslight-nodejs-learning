5eac8679a411d076d3db72f6e4ec027e93f74ea8
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-12-17 16:12:46 +0300

    lesson-5 done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
+import router from './routes/pageRoute.js';
 
 dotenv.config();
 
@@ -17,7 +18,6 @@ app.set('view engine', 'ejs');
 // Static Files middleware
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('index'));
-app.get('/about', (req, res) => res.render('about'));
+app.use('/', router);
 
 app.listen(port, () => console.log(`Application running on ${port}`));
